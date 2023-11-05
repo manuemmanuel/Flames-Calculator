c611df3915ccf7673b6985d6affe4fec6cdd6df6
Manu Emmanuel <manuemmanuel2026@cs.sjcetpalai.ac.in>
2023-11-05 19:15:44 +0530

    Update flamescheck.py

--- flamescheck.py
+++ flamescheck.py
@@ -1,37 +1,78 @@
 import streamlit as st
 
+def main():
+    st.title("Flames Game")
+    name = st.text_input("Enter your name: ").lower().strip()
+    crush = st.text_input("Enter your Crush's name: ").lower().strip()
+
+    if name and crush:
+        name_list = list_word(name)
+        crush_list = list_word(crush)
+
+        length_of_uncommon = find_length(name_list, crush_list)
+        flame = flames(length_of_uncommon)
+        output = check_condition(flame[0])
+
+        st.write(f"Result: {output}")
+
+        file_write(name, crush, output)
+
 def list_word(name):
     name_list = []
-
     for i in name:
         name_list.append(i)
     return name_list
 
-def find_length(name, crush):
+def find_length(name,crush):
     common_words=[]
+    repeating=[]
     if len(name)>len(crush):
+
         for i in crush:
             for j in name:
+
                 if i==j:
                     common_words.append(i)
                     break
     else:
         for i in name:
+            
             for j in crush:
+                
                 if i==j:
+
+                    
                     common_words.append(i)
                     break
+    
+    
+
+    for i in common_words:
+        if common_words.count(i)>1:
+            repeating.append(i)
+            common_words.remove(i)
+
 
     for i in common_words:
         name.remove(i)
         crush.remove(i)
 
+    for i in repeating:
+        if i in name and i in crush:
+            name.remove(i)
+            crush.remove(i)
+
+
+            
+
     length=len(name)+len(crush)
     return length
+
 
 def flames(value):
     flame=[]
     new_flame=[]
+
     flam='flames'
     for i in flam:
         flame.append(i)
@@ -76,6 +117,7 @@ def flames(value):
     return flame
 
 def check_condition(flame):
+
     if flame.lower().strip()=='f':
         return "FRIENDS"
     elif flame.lower().strip()=='l':
@@ -88,29 +130,12 @@ def check_condition(flame):
         return "ENEMY"
     elif flame.lower().strip()=='s':
         return "SIBLINGS"
-def file_write(name, crush, output):
-    with open("main.txt", "a") as file:
+
+def file_write(name,crush,output):
+
+    with open("main.txt","a") as file:
         file.write(f"{name},{crush},{output}\n")
 
-def main():
-    st.title("FLAMES Calculator")
-    
-    name = st.text_input("Enter your name:")
-    crush = st.text_input("Enter your crush's name:")
-    
-    if st.button("Calculate"):
-        name = name.lower().strip()
-        crush = crush.lower().strip()
-        name_list = list_word(name)
-        crush_list = list_word(crush)
-        
-        length_of_uncommon = find_length(name_list, crush_list)
-        flame_result = flames(length_of_uncommon)
-        output = check_condition(flame_result[0])
-
-        st.write(f"Result: {output}")
-        
-        file_write(name, crush, output)
 
 if __name__ == '__main__':
     main()
